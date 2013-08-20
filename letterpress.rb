2319b14cde5fe2fec4069eab05fa03208847d491
Matthew Bunday <zenceph@gmail.com>
2013-08-20 17:04:05 -0400

    filter by banned letters

--- letterpress.rb
+++ letterpress.rb
@@ -2,7 +2,13 @@ require "nokogiri"
 require "open-uri"
 
 class Letterpress
+    def initialize
+        @cache = {}
+    end
+
     def words_containing(letters)
+        letters = letters.chars.sort.join
+        return @cache[letters] if !@cache[letters].nil?
         doc = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/words-containing/#{letters}.html"))
         return nil if doc.text.match("No words found")
         words = doc.text.split("Words Found")[1].split("Search Again!")[0].split
@@ -13,6 +19,20 @@ class Letterpress
             words += doc.text.split("Words Found")[1].split("Search Again!")[0].split
             i += 1
         end
+        @cache[letters] = words
         words
+    end
+
+    def words(letters_contain, letters_not)
+        w = words_containing(letters_contain)
+        w.reject! {|word| contains_banned_letter(word, letters_not)}
+        w
+    end
+
+    def contains_banned_letter(word, letters)
+        letters.chars.each do |letter|
+            return true if word.include? letter
+        end
+        return false
     end
 end
