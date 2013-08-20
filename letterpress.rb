7f463b5e4bc0f5e355703bb94cb882202dbf73cb
Matthew Bunday <zenceph@gmail.com>
2013-08-20 16:45:25 -0400

    Finished words_containing method. Huzzah for scraping

--- letterpress.rb
+++ letterpress.rb
@@ -2,11 +2,17 @@ require "nokogiri"
 require "open-uri"
 
 class Letterpress
-
     def words_containing(letters)
         doc = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/words-containing/#{letters}.html"))
         return nil if doc.text.match("No words found")
-        doc.text.split("
+        words = doc.text.split("Words Found")[1].split("Search Again!")[0].split
+        i = 2
+        while true
+            doc = Nokogiri::HTML(open("http://www.wordhippo.com/what-is/words-containing/#{letters}.html?page=#{i}"))
+            break if doc.text.match("No words found")
+            words += doc.text.split("Words Found")[1].split("Search Again!")[0].split
+            i += 1
+        end
+        words
     end
-
 end
