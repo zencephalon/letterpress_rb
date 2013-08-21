24f6e71bccbed91dd27aa4f0fa8427b6cc662695
Matthew Bunday <zenceph@gmail.com>
2013-08-21 11:13:27 -0400

    Huge optimization.

--- letterpress.rb
+++ letterpress.rb
@@ -8,29 +8,35 @@ class Letterpress
         ('a'..'z').each do |letter|
             @letters_hash[letter] = letters.count letter
         end
+        @small_dict = @dict.reject {|word| no_letters(word)}.sort_by(&:length)
     end
 
     def words_containing(letters)
         return @cache[letters] if !@cache[letters].nil?
         letters_regex = /#{letters.chars.join(".*")}/
         words = []
-        @dict.each do |word|
+        @small_dict.each do |word|
             words << word if word.match(letters_regex)
         end
         @cache[letters] = words
     end
 
-    def words(letters_contain)
-        w = []
-        perms = letters_contain.chars.permutation.to_a
+    def words(letters_contain = nil)
+        return @small_dict if letters_contain.nil?
+
+        w, perms = [], letters_contain.chars.permutation.to_a
+
         puts "Checking #{perms.length} permutations:"
+
         i = 1
         perms.each do |perm|
             puts "#{i}: #{perm.join}"
             w += words_containing(perm.join)
             i += 1
         end
+
         return nil if w.empty?
+
         w.reject! {|word| no_letters(word)}
         w.sort_by(&:length)
     end
